f32428f6ab6d735f764de32a436ff520d660e429
manon.fouquet <manon.fouquet@artelys.com>
2020-11-03 14:50:30 +0100

    set up for webpack (dev server ok)

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { merge } = require('webpack-merge');
+
+//Development plugins
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+// Production plugins
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
+
+
+const commonConfig = {
+    entry: './src/client/index.js',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                exclude: /node_modules/,
+                loader: "file-loader",
+                options: {
+                  name: '[path][name].[ext]',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+};
+ 
+ const productionConfig = {}
+// const productionConfig = {
+    // mode: 'production',
+    // optimization: {
+        // minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    // },
+    // module: {
+        // rules: [
+            // {
+                // test: /\.scss$/,
+                // use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            // }
+        // ]
+    // },
+    // plugins: [
+        // new MiniCssExtractPlugin({filename: '[name].css'})
+    // ]
+// };
+ 
+const developmentConfig = {
+    mode: 'development',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+};
+
+ 
+ 
+module.exports = env => {
+    let conf={}
+    switch(env) {
+    case 'development':
+      conf =  merge(commonConfig, developmentConfig);
+      break;
+    case 'production':
+      conf =  merge(commonConfig, productionConfig);
+      break;
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+  console.log(":::: Webpack configuration used :::")
+  console.log(conf)
+  return conf
+}
